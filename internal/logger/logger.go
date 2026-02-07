0e30751c9387c016f95ef97decacf015926b755f
Justin Cordova <justinavodroc@gmail.com>
2026-02-07 14:45:01 -0500

    feat: create logger package with slog configuration

---
+++ internal/logger/logger.go
@@ -0,0 +1,65 @@
+package logger
+
+import (
+	"log/slog"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+func ConfigureFromFlags(cmd *cobra.Command) *slog.Logger {
+	debug, _ := cmd.Flags().GetBool("debug")
+	quiet, _ := cmd.Flags().GetBool("quiet")
+	logFile, _ := cmd.Flags().GetString("log-file")
+	jsonFormat, _ := cmd.Flags().GetBool("json")
+
+	level := levelFromFlags(debug, quiet)
+
+	var handler slog.Handler
+	if jsonFormat {
+		handler = slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{
+			Level: level,
+		})
+	} else {
+		handler = slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{
+			Level: level,
+			ReplaceAttr: func(groups []string, a slog.Attr) slog.Attr {
+				if a.Key == "source_path" {
+					a.Key = "src"
+				}
+				if a.Key == "repo_path" {
+					a.Key = "repo"
+				}
+				if a.Key == "backup_path" {
+					a.Key = "backup"
+				}
+				return a
+			},
+		})
+	}
+
+	if logFile != "" {
+		file, err := os.OpenFile(logFile, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0644)
+		if err != nil {
+			return slog.New(slog.NewTextHandler(os.Stderr, &slog.HandlerOptions{
+				Level: level,
+			}))
+		}
+		handler = slog.NewTextHandler(file, &slog.HandlerOptions{
+			Level: level,
+		})
+	}
+
+	return slog.New(handler)
+}
+
+func levelFromFlags(debug, quiet bool) slog.Level {
+	switch {
+	case debug:
+		return slog.LevelDebug
+	case quiet:
+		return slog.LevelError
+	default:
+		return slog.LevelInfo
+	}
+}
---
+++ internal/logger/logger_test.go
@@ -0,0 +1,60 @@
+package logger
+
+import (
+	"bytes"
+	"log/slog"
+	"testing"
+
+	"github.com/spf13/cobra"
+)
+
+func TestConfigureFromFlags(t *testing.T) {
+	cmd := &cobra.Command{}
+	cmd.PersistentFlags().Bool("debug", false, "")
+	cmd.PersistentFlags().Bool("quiet", false, "")
+	cmd.PersistentFlags().String("log-file", "", "")
+	cmd.PersistentFlags().Bool("json", false, "")
+
+	logger := ConfigureFromFlags(cmd)
+	if logger == nil {
+		t.Fatal("expected non-nil logger")
+	}
+}
+
+func TestLevelFromFlags(t *testing.T) {
+	tests := []struct {
+		name     string
+		debug    bool
+		quiet    bool
+		expected slog.Level
+	}{
+		{"default", false, false, slog.LevelInfo},
+		{"debug", true, false, slog.LevelDebug},
+		{"quiet", false, true, slog.LevelError},
+		{"debug overrides quiet", true, true, slog.LevelDebug},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := levelFromFlags(tt.debug, tt.quiet)
+			if got != tt.expected {
+				t.Errorf("levelFromFlags(%v, %v) = %v, want %v", tt.debug, tt.quiet, got, tt.expected)
+			}
+		})
+	}
+}
+
+func TestLogEmission(t *testing.T) {
+	var buf bytes.Buffer
+	handler := slog.NewTextHandler(&buf, &slog.HandlerOptions{
+		Level: slog.LevelDebug,
+	})
+	logger := slog.New(handler)
+
+	logger.Info("test message", "key", "value")
+
+	output := buf.String()
+	if len(output) == 0 {
+		t.Fatal("expected log output")
+	}
+}
